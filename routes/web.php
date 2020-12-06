be87f9c35d05f236f4da1b8667d06f4df7692236
gh0st <ghostofcordelia@protonmail.com>
2020-12-06 22:06:42 +0000

    refactor: Change route name to use laravel standard

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,13 @@ Auth::routes(['register' => false]);
 Route::group(['prefix' => 'backoffice', 'middleware' => ['auth']], function() {
     // backoffice route
     Route::get('/', 'DashboardController@index');
-    Route::get('dashboard',['as' => 'backoffice.dashboard','uses' => 'DashboardController@dashboard']);
+    Route::get('dashboard','DashboardController@dashboard')->name('backoffice.dashboard');
     Route::resource('users','UserController');
     Route::resource('permissions', 'PermissionController');
     Route::resource('roles', 'RoleController');
 
     // user Profile
-    Route::get('profile', ['as'=>'profile','uses'=>'UserController@profile']);
-    Route::patch('profile/{user}/update',['as' => 'profile.update', 'uses' => 'UserController@ProfileUpdate']);
-    Route::patch('profile/{user}/password',['as' => 'profile.password','uses' => 'UserController@ChangePassword']);
+    Route::get('profile', 'UserController@profile')->name('profile');
+    Route::patch('profile/{user}/update','UserController@ProfileUpdate')->name('profile.update');
+    Route::patch('profile/{user}/password','UserController@ChangePassword')->name('profile.password');
 });
