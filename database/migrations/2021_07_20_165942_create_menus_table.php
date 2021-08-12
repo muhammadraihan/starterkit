3f071eb86cbe5f40c376598fe4bd0cbfd270d3f8
gh0st <ghostofcordelia@protonmail.com>
2021-08-12 19:57:26 +0700

    chore: Add menu table migration

---
+++ database/migrations/2021_07_20_165942_create_menus_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->string('menu_title')->nullable();
+            $table->string('route_name')->nullable();
+            $table->string('icon_class')->nullable();
+            $table->bigInteger('parent_id')->default(0)->nullable();
+            $table->bigInteger('order')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('edited_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
